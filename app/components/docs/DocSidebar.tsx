3620c08cb8ed6ea9b2970aad0ebc6540ad633dff
pauljianbo <paul@greywolf.sg>
2025-05-19 17:52:39 +0800

    add in context documentation

--- app/components/docs/DocSidebar.tsx
+++ app/components/docs/DocSidebar.tsx
@@ -54,6 +54,32 @@ const navItems: NavItem[] = [
   },
   {
     label: {
+      en: 'Context',
+      zh: '上下文',
+    },
+    href: '/docs/context',
+    icon: <FaCogs className="text-doc_icon-light dark:text-doc_icon-dark" />,
+    more: [
+      {
+        label: {
+          en: 'BookSettingsContext',
+          zh: '书籍设置上下文',
+        },
+        href: '/docs/context/book-settings',
+        icon: <FaCogs className="text-doc_icon-light dark:text-doc_icon-dark" />,
+      },
+      {
+        label: {
+          en: 'ThemeContext',
+          zh: '主题上下文',
+        },
+        href: '/docs/context/theme',
+        icon: <FaCogs className="text-doc_icon-light dark:text-doc_icon-dark" />,
+      },
+    ],
+  },
+  {
+    label: {
       en: 'UI Components',
       zh: 'UI 组件',
     },
@@ -70,6 +96,7 @@ const navItems: NavItem[] = [
       },
     ],
   },
+
   {
     label: {
       en: 'Development',
